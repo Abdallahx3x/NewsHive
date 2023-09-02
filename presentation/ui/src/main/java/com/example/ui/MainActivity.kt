7c60d1820c377847391d24616a30edf28a45e6d9
Abdallah Sayed <abdullahsayed3355@gmail.com>
2023-09-02 03:28:04 +0300

    refactor main activity

--- presentation/ui/src/main/java/com/example/ui/MainActivity.kt
+++ presentation/ui/src/main/java/com/example/ui/MainActivity.kt
@@ -2,24 +2,17 @@ package com.example.ui
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import com.example.ui.theme.NewsHiveTheme
-import com.example.viewmodel.NewsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-   private val newsViewModel:NewsViewModel by viewModels()
 
     @SuppressLint("StateFlowValueCalledInComposition")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,14 +20,10 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             NewsHiveTheme {
-                val stat3e by newsViewModel.state.collectAsState()
-                Log.d("testAboood",stat3e)
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
-                    Text(stat3e)
                 }
             }
         }
