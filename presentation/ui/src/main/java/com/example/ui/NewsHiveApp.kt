29bd6e6dcc7d55d38c5d9a97a19a7bca5069c901
Abdallah Sayed <abdullahsayed3355@gmail.com>
2023-09-18 14:57:10 +0300

    add discover screen to bottom navigation and change the color of the status bar

--- presentation/ui/src/main/java/com/example/ui/NewsHiveApp.kt
+++ presentation/ui/src/main/java/com/example/ui/NewsHiveApp.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.rememberNavController
 import com.example.ui.screens.bottomNavigation.BottomNavigation
 import com.example.ui.screens.bottomNavigation.currentRoute
 import com.example.ui.screens.bottomNavigation.BottomNavigationItem
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -18,9 +20,12 @@ fun NewsHiveApp() {
     val navController = rememberNavController()
     val shouldShowBottomNavigation = when (currentRoute(navController)) {
         BottomNavigationItem.Home.screenRoute,
+        BottomNavigationItem.Discover.screenRoute
         -> true
         else -> false
     }
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setSystemBarsColor(Color.White,darkIcons = true)
 
     Scaffold(bottomBar = { BottomNavigation(navController = navController) }) {paddingValue->
         Box (modifier = Modifier.padding(paddingValue)){
--- presentation/ui/src/main/java/com/example/ui/NewsHiveNavGraph.kt
+++ presentation/ui/src/main/java/com/example/ui/NewsHiveNavGraph.kt
@@ -3,12 +3,14 @@ package com.example.ui
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import com.example.ui.screens.composable.homeRoute
+import com.example.ui.screens.discover.discoverRoute
+import com.example.ui.screens.home.homeRoute
 
 
 @Composable
 fun NewsHiveNavGraph(navController: NavHostController) {
     NavHost(navController = navController, startDestination = Screen.Home.route) {
         homeRoute()
+        discoverRoute()
     }
 }
