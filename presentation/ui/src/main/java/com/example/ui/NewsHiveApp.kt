6258534c57c18cb76c12f56126745e13735f5491
Abdallah Sayed <abdullahsayed3355@gmail.com>
2023-12-13 14:23:27 +0200

    Delete unnecessary imports in NewsHiveApp

--- presentation/ui/src/main/java/com/example/ui/NewsHiveApp.kt
+++ presentation/ui/src/main/java/com/example/ui/NewsHiveApp.kt
@@ -1,18 +1,15 @@
 package com.example.ui
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.rememberNavController
 import com.example.ui.screens.bottomNavigation.BottomNavigation
 import com.example.ui.screens.bottomNavigation.BottomNavigationItem
 import com.example.ui.screens.bottomNavigation.currentRoute
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
